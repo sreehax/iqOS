c41cab70ac9dec7bf626dcd64eff2f24a580fb51
Sreehari S <sreehari@sreehari.dynu.net>
2018-12-30 17:16:25 -0800

    initial commit

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source functions.sh
+# DEFINITIONS
+export KERNEL_VERSION=4.20
+export OS_VERSION=0.0.1
+export TOP=$PWD
+export URL=https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-$KERNEL_VERSION.tar.gz
+fetchkernel
+fetchbusybox
+buildkernel
+buildbusybox
+packer
+buildiso
+cd $TOP
---
+++ functions.sh
@@ -0,0 +1,87 @@
+fetchkernel() {
+	cd $TOP
+	mkdir -p work/out
+	rm -rf work/*
+	cd work
+	mkdir out
+	curl -o linux.tar.gz $URL
+	tar -xf linux.tar.gz
+	rm linux.tar.gz
+	mv linux* linux
+	cd linux
+	make mrproper
+}
+fetchbusybox() {
+	cd $TOP
+	if [ -d work/busybox ]; then
+		cd work/busybox
+		git pull
+		make mrproper
+	else
+		cd work
+		git clone git://busybox.net/busybox.git
+		cd busybox
+		make mrproper
+	fi
+
+}
+buildkernel() {
+	cd $TOP
+	cp files/linux-config work/linux/.config
+	cd work/linux
+	make -j`nproc`
+	cp arch/x86/boot/bzImage ../out/vmlinuz
+}
+buildbusybox() {
+	cd $TOP
+	cp files/busybox-config work/busybox/.config
+	cd work/busybox
+	make -j`nproc`
+	cp busybox ../out/
+}
+packer() {
+	cd $TOP
+	mkdir -p work/rootfs
+	cd work/rootfs
+	rm -rf *
+	mkdir dev sys proc lib bin sbin usr usr/bin usr/sbin etc
+	ln -s lib lib64
+	ln -s ../lib usr/lib
+	ln -s ../lib usr/lib64
+	cp ../out/busybox bin/
+	chroot . /bin/busybox --install -s
+	rm linuxrc
+	cd ../linux
+	make modules_install INSTALL_MOD_PATH=`realpath ../rootfs`
+	cd ../rootfs
+	cp ../../files/welcome etc/
+	cp ../../files/init .
+	chmod +x init
+	find . | cpio -H newc -o | gzip > ../out/initrd.img
+}
+buildiso() {
+	cd $TOP
+	mkdir -p work/efidir work/isodir
+	cd work/efidir
+	rm -rf *
+	cd $TOP
+	dd bs=1M count=32 if=/dev/zero of=work/out/efi.img
+	mkfs.vfat work/out/efi.img
+	mount -t vfat work/out/efi.img work/efidir
+	cd work/efidir
+	mkdir -p EFI/BOOT boot/grub
+	cp ../out/vmlinuz ../out/initrd.img boot/
+	cp ../../files/*.EFI EFI/BOOT/
+	cp ../../files/grub.cfg boot/grub
+	cd ..
+	umount efidir
+	mkdir -p isodir
+	cd isodir
+	rm -rf *
+	mkdir isolinux
+	cp -r ../../files/isolinux.* ../../files/ldlinux.c32 isolinux/
+	cp -r ../out/efi.img .
+	mkdir boot
+	cp ../out/vmlinuz ../out/initrd.img boot/
+	xorriso -as mkisofs -isohybrid-mbr ../../files/isohdpfx.bin -c isolinux/boot.cat -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot -e efi.img -no-emul-boot -isohybrid-gpt-basdat -o $TOP/linux.iso  .
+}
